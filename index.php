46a5a4036133827af0212233b6feb3ac1e0336d0
Farhan <118242884+FarhanGinting@users.noreply.github.com>
2025-05-09 05:52:04 +0700

    feat (Route Branch) : menambahkan route branch ke index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Landing Page</title>
+</head>
+<body>
+    <h1>Ini Landing Page Website</h1>
+    <h4>Route List</h4>
+    <ul>
+        <li><a href="views/pages/admin/branch/index.php">Branch ✅</a></li>
+    </ul>
+    
+</body>
+</html>
